a5b0cb0febf65431f9adc42c2755eff603f7d5c1
Ricardo Costa <rkosta@gmail.com>
2024-03-05 15:08:02 +0000

    environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { BookingsModule } from './bookings/bookings.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CurrentUserMiddleware } from './middleware/current-user.middleware';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmConfigService } from './type-orm-config.service';
 import { PermissionsModule } from './permissions/permissions.module';
 
@@ -14,7 +14,7 @@ import { PermissionsModule } from './permissions/permissions.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
     }),
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService,
